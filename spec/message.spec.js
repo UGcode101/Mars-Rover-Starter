7035a17c3db1bc7947cdd027f665272fb2c99992
UGcode101 <142420124+UGcode101@users.noreply.github.com>
2024-02-10 21:46:10 -0600

    task 3 complete

--- spec/message.spec.js
+++ spec/message.spec.js
@@ -16,4 +16,14 @@ describe("Message class", () => {
     const message = new Message(name);
     expect(message.name).toBe(name);
   });
+
+  it("contains a commands array passed into the constructor as the 2nd argument", () => {
+    const name = "Test Commands array";
+    const commands = [
+      new Command("MODE_CHANGE", "LOW_POWER"),
+      new Command("STATUS_CHECK"),
+    ];
+    const message = new Message(name, commands);
+    expect(message.commands).toEqual(commands);
+  });
 });
