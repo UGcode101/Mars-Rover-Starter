d36f7220bc0ee6385d1ca9c911de8f5953b8bcb6
UGcode101 <142420124+UGcode101@users.noreply.github.com>
2024-02-15 18:26:18 -0600

    clean up comments for readability

--- spec/command.spec.js
+++ spec/command.spec.js
@@ -1,8 +1,5 @@
 const Command = require("../command.js");
 
-// NOTE: If at any time, you want to focus on the output from a single test, feel free to comment out all the others.
-//       However, do NOT edit the grading tests for any reason and make sure to un-comment out your code to get the autograder to pass.
-
 describe("Command class", () => {
   it("throws error if a command type is NOT passed into the constructor as the first parameter", () => {
     expect(() => {
--- spec/message.spec.js
+++ spec/message.spec.js
@@ -1,9 +1,6 @@
 const Message = require("../message.js");
 const Command = require("../command.js");
 
-// NOTE: If at any time, you want to focus on the output from a single test, feel free to comment out all the others.
-//       However, do NOT edit the grading tests for any reason and make sure to un-comment out your code to get the autograder to pass.
-
 describe("Message class", () => {
   it("throws error if a name is NOT passed into the constructor as the first parameter", () => {
     expect(() => {
